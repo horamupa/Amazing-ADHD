c75d8bc3d7cff3fae78b05071e831e9a1cbe11f2
horamupa <horamupa@gmail.com>
2022-11-02 14:50:24 +0400

    Initial Commit

---
+++ Amazing ADHD/Amazing_ADHDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Amazing_ADHDApp.swift
+//  Amazing ADHD
+//
+//  Created by MM on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Amazing_ADHDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
